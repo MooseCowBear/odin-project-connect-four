e55cf789ba0cefd62bcf557d203d161da02266f7
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-11 12:08:51 -0400

    Test #get_starting_board indirectly through test for #initialize

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -2,19 +2,15 @@ require_relative '../lib/connect_four.rb'
 
 describe ConnectFour do
   describe '#initialize' do
-    it 'calls get_starting_board' do
-
-    end
-  end
-
-  describe '#get_starting_board' do
     subject(:starting_game) { described_class.new  }
 
-    it 'returns an empty board' do
-      empty_board = Array.new(6) { Array.new(7) } #should be 6 rows, 7 cols
+    it 'default returns empty' do
+      empty_board = Array.new(6) { Array.new(7) } 
       board = starting_game.board
       expect(board).to eq(empty_board)
     end
+
+    
   end
 
 
