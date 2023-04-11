0849996702dff5e5e6568f7c04cc1af4d3128c4a
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-11 12:37:25 -0400

    Edit #initialize

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -2,10 +2,10 @@
 class ConnectFour
   attr_accessor :player1, :player2, :board, :curr_player
 
-  def initialize(player1 = nil, player2 = nil, board = get_starting_board, curr_player = rand(1..2))
+  def initialize(board = get_starting_board, curr_player = rand(1..2))
     @winner = nil
-    @player1 = player1
-    @player2 = player2
+    @player1 = nil
+    @player2 = nil
     @board = board
     @curr_player = curr_player
   end
